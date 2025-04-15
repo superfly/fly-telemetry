f053b47a0863c2918a5b9b9dde851ecfc48619c3
Will Jordan <will.jordan@gmail.com>
2025-04-15 13:12:45 -0700

    require access token in main process

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+: "${ACCESS_TOKEN?}"
 export enableTCP6=true
 /victoria-metrics-prod -envflag.enable -storageDataPath /data/metrics &
 /victoria-logs-prod -envflag.enable -storageDataPath /data/logs &
